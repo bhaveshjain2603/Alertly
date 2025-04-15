c30b37f9f3a361e64a37a02f099c719930337940
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2025-04-15 18:10:09 +0530

    Initialized controller

---
+++ server/controllers/User.controllers.js
@@ -0,0 +1,27 @@
+import User from "../models/User.models.js";
+
+export const registerUser = async (req, res) => {
+    const { name, age, location, familyContact, message } = req.body;
+    try {
+      const newUser = new User({ name, age, location, familyContact, message });
+      await newUser.save();
+      res.json({ message: "User data saved successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: "Error saving user data." });
+    }
+};
+  
+export const sendAlerts = async (req, res) => {
+    const { name, location, familyContact, message } = req.body;
+    try {
+      await transporter.sendMail({
+        from: process.env.SMTP_EMAIL,
+        to: familyContact,
+        subject: "Emergency Alert",
+        text: `An emergency alert has been triggered by ${name}. \nCurrent location: ${location}. \nMessage: ${message}`
+      });
+      res.json({ message: "Emergency alert sent to family members!" });
+    } catch (error) {
+      res.status(500).json({ message: "Error sending alert." });
+    }
+};
