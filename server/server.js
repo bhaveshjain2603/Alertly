247a9a7a143c2b4dffd1ac3b80d204d02b931d59
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2025-04-16 17:47:17 +0530

    Update server file

--- server/server.js
+++ server/server.js
@@ -11,13 +11,7 @@ const app = express();
 dotenv.config();
 
 // Middleware
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
