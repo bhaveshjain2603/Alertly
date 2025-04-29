ccabb3e11036dc7d182605e7f53cf2d635f24c19
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2025-04-29 21:39:28 +0530

    Removed the user endpoint

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -1,10 +1,8 @@
-import { registerUser, sendAlerts } from "../controllers/User.controllers.js";
+import { sendAlerts } from "../controllers/User.controllers.js";
 
 import express from "express";
 const router = express.Router();
 
-router.post("/users", registerUser);
-  
 router.post("/alert", sendAlerts);
 
 export default router;
